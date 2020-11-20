e1759b388acdd9f8846f9a6467c7922ac65955a3
douglaslmts <douglongaray@gmail.com>
2020-11-20 15:47:49 -0300

    ajustando ERP

--- loja/ERP/views.py
+++ loja/ERP/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 
 def home_erp(request):
-    return HttpResponse("Pagina de inicio ERP")
+    return render(request, 'ERP/index.html')
